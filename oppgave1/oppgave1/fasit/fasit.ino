4d50fffec5500f6fd15f2b72ea470dff8bd2a389
Hackerspace VR <hackerspacevr@gmail.com>
2018-10-14 10:43:27 +0200

    Add task 1 Hello World

---
+++ oppgave1/oppgave1/fasit/fasit.ino
@@ -0,0 +1,14 @@
+
+const int ledPin = 10;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);   
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH);
+  delay(250); 
+  digitalWrite(ledPin, LOW);
+  delay(250);
+}
+
---
+++ oppgave1/oppgave1/oppgave1.ino
@@ -0,0 +1,21 @@
+
+// Use this pin on your board 
+const int ledPin = 10; 
+
+
+void setup() {
+  // put your setup code here, to run once:
+  // Declare what the ledPin should do
+
+  // YOUR CODE HERE
+
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // Logic for blinking goes here 
+
+  // YOUR CODE HERE 
+ 
+}
