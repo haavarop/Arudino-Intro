0a5d5504de430d24d2dc916b10a535fc5f58de69
Hackerspace VR <hackerspacevr@gmail.com>
2018-10-31 11:21:18 +0100

    Add task 1c: RGB button control

---
+++ oppgave1/oppgave1c/oppgave1c.ino
@@ -0,0 +1,21 @@
+// Use buttons or switches to turn on different colors of light
+const int redPin   = 11; 
+const int greenPin = 12;
+const int bluePin  = 13;  
+const int delayValue = 2000; 
+
+// Add extra variables for buttons here 
+
+void setup() {
+  pinMode(redPin,   OUTPUT);
+  pinMode(bluePin,  OUTPUT);
+  pinMode(greenPin, OUTPUT);
+
+  /* SETUP CODE FOR BUTTONS HERE */ 
+  
+}
+
+void loop() {
+
+  /* YOUR CODE HERE */ 
+}
